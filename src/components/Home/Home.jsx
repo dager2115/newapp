d2a6dca676221f50b7aae6f37039310b2c2f7a4b
dager2115 <dager2115@gmail.com>
2021-08-30 22:39:52 -0500

    subo cambios y datos actualizados

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,27 @@ import React, { useState } from 'react'
 import './home.css'
 import { db } from '../../firebase'
 import { useParams } from 'react-router-dom'
+import datos from './9gemc-cjxwb.json'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const producto = {
-    nombre: "mafe",
-    apellido: "caro",
-    edad: "18"
-}
+const productos = [
+    {
+        nombre: "carne",
+        precio: "18000",
+    },
+    {
+        nombre: "filete",
+        precio: "15000",
+    },
+    {
+        nombre: "limonada",
+        precio: "6000",
+    },
+    {
+        nombre: "pollo",
+        precio: "20000",
+    },
+]
 
 const Home = () => {
 
@@ -25,9 +40,15 @@ const Home = () => {
         }
     }
     const { id } = useParams()
-    const [pedido, setPedido] = useState([producto])
+    const [pedido, setPedido] = useState([])
     const [formulario, setFormulario] = useState(initialValues.formulario)
+    const [modal, setModal] = useState(false);
 
+    const toggle = () => setModal(!modal);
+    // const total = pedido.forEach( producto => { 
+    //     let suma 
+    //     producto
+    // })
     const [checked, setChecked] = useState(initialValues.checked)
 
     const handlesubmit = async (e) => {
@@ -36,7 +57,7 @@ const Home = () => {
         pedido.forEach(elemento => {
             Object.assign(
                 objetoPedido, 
-                {[elemento.nombre]:{apellido: elemento.apellido, edad: elemento.edad}}
+                {[elemento.nombre]:{precio: elemento.precio, cantidad: elemento.cantidad}}
                 )
         })
         console.log(objetoPedido)
@@ -46,8 +67,12 @@ const Home = () => {
             setFormulario(initialValues.formulario)
             setChecked(initialValues.checked)
         })
-        .catch(err=> alert("algo salio mal"))
+        .catch(err=> 
+            alert("algo salio mal")
+        )
     } 
+
+    
 
     const handleChange = (e) => {
         setFormulario({
@@ -87,12 +112,40 @@ const Home = () => {
         }
     }
 
+    const handleAddGirls = () => {
+        console.log(datos)
+        datos.forEach(async dato => {
+            await db.collection('asistentes').doc().set(
+                {
+                    ...dato,
+                    rol: "graduando"
+                }
+            )
+        .then(res => {
+            console.log("agregadisimo")
+        })
+        .catch(err=> alert("algo salio mal"))
+        })
+    }
+
+    const filtro =  async () => {
+        await db.collection('asistentes').where('nombre', '==', formulario.nombre).get()
+        .then(res => {
+            res.docs.forEach(dato => {
+                console.log(dato.data())
+            })
+        })
+    }
+
+    const agregar = (producto) => {
+        setPedido([...pedido, {...producto, cantidad:1}])
+    }
+
 return(
     <div>
-        <h1>{id}</h1>
         <form onSubmit={handlesubmit} className="formulario">
             <input name="nombre" className="input" placeholder="Incerte su nombre" value={formulario.nombre} onChange={handleChange}/>  
-            <input name="apellido" className="input" placeholder="Incerte su apellido" value={formulario.apellido} onChange={handleChange}/>
+            {/* <input name="apellido" className="input" placeholder="Incerte su apellido" value={formulario.apellido} onChange={handleChange}/>
             <input name="edad" className="input" placeholder="Incerte su edad" value={formulario.edad} onChange={handleChange}/>
             <div>
             <input type="radio" name="yes" checked={checked.yes} onChange={handleChecked}/><label>si</label>
@@ -100,16 +153,10 @@ return(
             </div>
             <button type="submit" className="enviar">enviar</button>
             <button type="button" className="enviar" onClick={addProducto}>agregar</button>
-        </form>
-        <div>
-            {pedido?.map((elem, index) => 
-                <div key={index}>
-                    <h3>{elem.nombre}</h3>
-                    <p>{elem.apellido}</p>
-                    <p>{elem.edad}</p>
-                </div>
-            )}
-        </div>
+<input type="file" id="documento" accept=".xls,.xlsx"/> */}
+            <button type="button" onClick={handleAddGirls}>subir archivo</button> 
+            {/* <button type="button" onClick={filtro}>traer</button> */}
+        </form> 
     </div>
 )
 }
